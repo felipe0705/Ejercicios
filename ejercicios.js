ec1bcde99a92c016c3e1ea8afaccd044b2ec9163
felipe0705 <fonsecafelipe168@gmail.com>
2023-11-22 17:47:58 -0500

    Changes to be committed: 	new file:   ejercicios.js 	new file:   index.html

---
+++ ejercicios.js
@@ -0,0 +1,28 @@
+
+///ejercicio 1
+document.getElementById('calcular').addEventListener('click', function() {
+    var peso = document.getElementById('peso').value;
+    var altura = document.getElementById('altura').value;
+    var imc = peso / (altura * altura);
+    document.getElementById('resultado').value = imc.toFixed(2);
+});
+
+
+
+
+
+////// ejercicio 2
+var usdInput = document.getElementById('usd');
+var copInput = document.getElementById('cop');
+
+usdInput.addEventListener('input', function() {
+    var usd = this.value;
+    var cop = usd * 4000;
+    copInput.value = cop;
+});
+
+copInput.addEventListener('input', function() {
+    var cop = this.value;
+    var usd = cop / 4000;
+    usdInput.value = usd;
+});
